e668dd345d1a732b1b2b96fd79de0ae2d0c066e8
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-03-31 16:10:29 +0200

    Es. completo: colori assegnati per tipo, filtro per tipo funzionante

--- js/script.js
+++ js/script.js
@@ -65,16 +65,73 @@ function iconsPrint(iconList) {
     let container = $('main');
 
     iconList.forEach(element => {
-        container.append(`<div><i class="${element.family} ${element.prefix + element.name}"</i></div>`);
+        container.append(`
+             <div>
+             <i style="color: ${element.color}" class="${element.family} ${element.prefix + element.name}"></i>
+                 
+             <h5>${element.name.charAt(0).toUpperCase() + element.name.slice(1)}</h5>
+             </div>`
+        );
     });
+}
+
+// Funzione che torna i colori
+function getColors() {
+    const colors = ['dodgerblue', 'greenyellow', 'darkorchid', 'red'];
+    return colors;
+}
+
+// Funzione che assegna i colori in base al tipo
+function colorAssign(iconList, colors) {
+    const types = iconList.map((element) => element.type);
+    let typeList = [];
+
+    types.forEach((element) => {
+        console.log(element);
+        if(typeList.includes(element) == false){
+            typeList.push(element);
+        }
+    });
+
+    iconList.forEach((element, index) => {
+        element.color = colors[typeList.indexOf(element.type)];
+    });
+    return iconList;
+}
+
+// Funzione cambio selezione filtro
+function selectChange(iconList) {
+    
+    $('#iconType').change(function() {
+        let newIconList = [];
+        let selected = $(this);
+        if(selected.val() == false){
+            $('main').html('');
+            iconsPrint(iconList);
+        }else{
+            $('main').html('');
+            iconList.forEach(element => {
+                if(element.type == selected.val())
+                    newIconList.push(element);
+            });
+        }
+        iconsPrint(newIconList);
+    });
+    
 }
 
 // Funzione principale
 function init() {
-    const iconList = getIconList();
+    let iconList = getIconList();
     console.log(iconList);
 
+    const colors = getColors();
+    
+    iconList = colorAssign(iconList, colors);
+
     iconsPrint(iconList);
+
+    selectChange(iconList);
 }
 
 $(init);
